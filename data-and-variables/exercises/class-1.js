ee1538ef3172a4c36fe3472326706c8921f3935e
Jak W <kussman.johna@gmail.com>
2024-04-29 21:43:12 -0500

    Class 2 exercise conditionals added

---
+++ data-and-variables/exercises/class-1.js
@@ -0,0 +1,22 @@
+/*let firstName = "Johna";
+console.log(firstName);
+
+firstName = "Todd";
+console.log(firstName);*/
+
+/*const firstNumber = 5;
+const secondNumber = 3;
+console.log(3 * 4);
+console.log(firstNumber * secondNumber);*/
+
+//const input = require('readline-sync');
+//const firstName = input.question("What is your name? ");
+
+
+let num = 0;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
